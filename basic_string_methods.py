b645e1ca720ab58b0a4ccab6dab63c6741292ee9
Ida <ida.starek@gmail.com>
2024-06-20 23:19:08 +0100

    added basic_string_methods

---
+++ basic_string_methods.py
@@ -0,0 +1,11 @@
+# Asks the user to input a value.
+var = input("Please enter a value: ")
+
+# Prints the input value in uppercase.
+print(var.upper())
+
+# Prints the length of the input value.
+print(len(var))
+
+# Checks and prints whether the input value is numeric.
+print(var.isnumeric())
